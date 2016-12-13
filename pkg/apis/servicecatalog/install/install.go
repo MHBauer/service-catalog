ed32b9345e4f41d6a4e101ec5d3c5ddbd942c217
Morgan Bauer <mbauer@us.ibm.com>
2016-12-12 17:37:34 -0800

    new extra stuff

---
+++ pkg/apis/servicecatalog/install/install.go
@@ -0,0 +1,26 @@
+package install
+
+import (
+	"k8s.io/kubernetes/pkg/apimachinery/announced"
+	"k8s.io/kubernetes/pkg/util/sets"
+
+	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog"
+	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1alpha1"
+)
+
+func init() {
+	if err := announced.NewGroupMetaFactory(
+		&announced.GroupMetaFactoryArgs{
+			GroupName:                  servicecatalog.GroupName,
+			VersionPreferenceOrder:     []string{v1beta1.SchemeGroupVersion.Version},
+			ImportPrefix:               "k8s.io/kubernetes/pkg/apis/extensions",
+			RootScopedKinds:            sets.NewString("service-catalog"),
+			AddInternalObjectsToScheme: servicecatalog.AddToScheme,
+		},
+		announced.VersionToSchemeFunc{
+			v1alpha1.SchemeGroupVersion.Version: v1alpha1.AddToScheme,
+		},
+	).Announce().RegisterAndEnable(); err != nil {
+		panic(err)
+	}
+}
---
+++ pkg/apis/servicecatalog/register.go
@@ -0,0 +1,3 @@
+package servicecatalog
+
+const GroupName = "extensions"
