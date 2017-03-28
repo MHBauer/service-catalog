f9f4f1cfa096bfb9dc1285bf3c5814d326118c13
Morgan Bauer <mbauer@us.ibm.com>
2017-03-27 18:09:11 -0700

    print mounts and make root shared

--- test/e2e.sh
+++ test/e2e.sh
@@ -44,6 +44,8 @@ kubectl () {
 #trap cleanup EXIT
 
 sudo mkdir -p /var/lib/kubelet
+mount
+mount --make-shared /
 #start the localkube in docker
 docker run --name svc-cat-lk -d -it --privileged --volume=/:/rootfs:ro --volume=/sys:/sys:rw     --volume=/var/lib/docker/:/var/lib/docker:rw --volume=/var/lib/dockershim/sandbox:/var/lib/dockershim/sandbox:rw     --volume=/var/lib/kubelet/:/var/lib/kubelet:rw,shared     --volume=/var/run:/var/run:rw     --net=host -P brahmaroutu/localkube ./localkube --containerized
 
